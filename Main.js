ed7c4c1c70fe20d8d87368c010b313455457146e
JustABrokeGuy <101163929+JustABrokeGuy@users.noreply.github.com>
2026-05-25 17:25:44 +0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,24 @@
+// Variables.
+let images = ["images/main_page/Porsche_911.png", "images/main_page/Toyota_hilux.jpg"];
+let numberOfImages = images.length;
+let i = 0;
+
+// Main function that cycles through the images.
+// Each images has 3 seconds before being changed.
+setInterval(function() {
+
+    let img = document.getElementById("Images");
+    
+    let src = images[i];
+
+    img.src = src;
+
+    i++;
+
+    if (i == numberOfImages) {
+
+        i = 0;
+
+    }
+
+}, 3000);
